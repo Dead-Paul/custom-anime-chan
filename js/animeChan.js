a074d091676cf241e163e0b668dce5ed9b0854d8
Dead Paul <122644231+Dead-Paul@users.noreply.github.com>
2024-12-30 21:57:02 +0100

    Changing comments, adding ts-check, function setActive for setting active type of the part of the character, startPosition object for saving all starting points for the parts of the character body

--- js/animeChan.js
+++ js/animeChan.js
@@ -1,11 +1,21 @@
+//@ts-check
+
+const 
+    /** Path for all img sources @type {String}*/
+    path = './img/anime-chan/',
+    /**Id of the container with anime character @type {String}*/
+    containerId = 'anime-chan-container'
+;
+ 
 /**
  * Creates new Image
  * @function
  * @param {!String} src - Path to Image
- * @param {?String} position - CSS style position ('absolute' by default) 
- * @returns {Image}
+ * @param {?String} position - CSS style position ('absolute' by default)
+ * @returns {HTMLImageElement}
  */
-const createImage = (src, position = 'absolute') => {
+function createImage (src, position) {
+    position = position || 'absolute';
     const img = new Image();
     img.src = src;
     img.style.position = position;
@@ -13,108 +23,175 @@ const createImage = (src, position = 'absolute') => {
 };
 
 /**
+ * Adds prefix to the word
+ * @function
+ * @param {String} prefix - Prefix to add
+ * @param {String} word - Word to add prefix to
+ * @returns {String} - Word with prefix
+ */
+function addPrefix(prefix, word) {
+    return prefix + word.charAt(0).toUpperCase() + word.slice(1);
+};
+
+
+/**
  * Contains all important info about Anime character
- * @class 
- * @constructor
- * @property {Object} part - Parts info
- * @property {Object} part.body - Body info
- * @property {Image} part.body.mainSrc - Source of the main body image
- * 
- * @property {Object} part.sclera - Sclera info
- * @property {String} part.sclera.mainSrc - Source of the main sclera image
- * 
- * @property {Object} part.eyes - Eyes info (pupil and glare)
- * @property {String} part.eyes.mainSrc - Source of the main eyes image
- * 
- * @property {Number} ratio - How much bigger or smaller has the image become on the page
- * @property {Object} activeSrc - Contains fields of active image sources for every character part
+ * @class
+ *
+ * @property {function} setActive - Sets active part of the main character on another part
  */
 class AnimeChan {
     /**
      * All important info about anime character (all fields with 'Src' in the end of the name counts as src for images)
+     * @constructor
+     *
      * @param {Object} character - Character info
      * @param {Object} character.body - Body info
-     * @param {String} character.body.mainSrc - Source of the main body image
-     * 
+     * @param {Object} character.body.main - Main body image
+     * @param {String} character.body.main.src - Source of the main body image
+     * @param {Object} character.body.main.position - Position of the body (left top corner)
+     * @param {Number} character.body.main.position.x - X (horizontal) position of the body
+     * @param {Number} character.body.main.position.y - Y (vertical) position of the body
+     *
      * @param {Object} character.sclera - Sclera info
-     * @param {String} character.sclera.mainSrc - Source of the main sclera image
-     * @param {Object} character.sclera.position - Position of the sclera (left top corner)
-     * @param {Number} character.sclera.position.x - X (horizontal) position of the sclera
-     * @param {Number} character.sclera.position.y - Y (vertical) position of the sclera
-     * 
+     * @param {Object} character.sclera.main - Main sclera image
+     * @param {String} character.sclera.main.src - Source of the main sclera image
+     * @param {Object} character.sclera.main.position - Position of the sclera (left top corner)
+     * @param {Number} character.sclera.main.position.x - X (horizontal) position of the sclera
+     * @param {Number} character.sclera.main.position.y - Y (vertical) position of the sclera
+     *
      * @param {Object} character.eyes - Eyes info (pupil and glare)
-     * @param {String} character.eyes.mainSrc - Source of the main eyes image
-     * @param {Object} character.eyes.position - Position of the eyes (left top corner)
-     * @param {Number} character.eyes.position.x - X (horizontal) position of the eyes
-     * @param {Number} character.eyes.position.y - Y (vertical) position of the eyes
+     * @param {Object} character.eyes.main - Main eyes image
+     * @param {String} character.eyes.main.src - Source of the main eyes image
+     * @param {Object} character.eyes.main.position - Position of the eyes (left top corner)
+     * @param {Number} character.eyes.main.position.x - X (horizontal) position of the eyes
+     * @param {Number} character.eyes.main.position.y - Y (vertical) position of the eyes
+     *
+     * @param {Object} character.mouth - Mouth info (pupil and glare)
+     * @param {Object} character.mouth.main - Main mouth image
+     * @param {String} character.mouth.main.src - Source of the main mouth image
+     * @param {Object} character.mouth.main.position - Position of the mouth (left top corner)
+     * @param {Number} character.mouth.main.position.x - X (horizontal) position of the mouth
+     * @param {Number} character.mouth.main.position.y - Y (vertical) position of the mouth
      */
     constructor(character) {
+        /**
+         * Parts info
+         * @type {Object}
+         *
+         * @property {Object} part.body - Body info
+         * @property {HTMLImageElement} part.body.main - Source of the main body image
+         *
+         * @property {Object} part.sclera - Sclera info
+         * @property {HTMLImageElement} part.sclera.main - Source of the main sclera image
+         *
+         * @property {Object} part.eyes - Eyes info (pupil and glare)
+         * @property {HTMLImageElement} part.eyes.main - Source of the main eyes image
+         *
+         * @property {Object} part.mouth - Eyes info (pupil and glare)
+         * @property {HTMLImageElement} part.mouth.main - Source of the main eyes image
+         *
+         */
         this.part = Object();
-        this.activeSrc = Object();
-        Object.keys(character).forEach(partName => {
+        /**
+         * Contains fields of active image for every character part
+         * @type {Object}
+         */
+        this.active = Object();
+        /**
+         * Contains start positions for all types of all parts of the character
+         * @type {Object}
+         */
+        this.startPosition = Object();
+        Object.keys(character).forEach((partName) => {
             this.part[partName] = Object();
-            this.activeSrc[partName] = 'mainSrc';
-            Object.keys(character[partName]).forEach((partImage, i) => {
-                if (partImage.endsWith('Src')) {
-                    this.part[partName][partImage] = createImage(character[partName][partImage]);
-                    if (Object.keys(character[partName]).indexOf('position') > -1) {
-                        this.part[partName][partImage].style.left = `${character[partName].position.x}px`;    
-                        this.part[partName][partImage].style.top = `${character[partName].position.y}px`;
-                    this.part[partName][partImage].setAttribute('id', `anime-chan-${partName}`);
-                    }
-                }
+            this.active[partName] = 'main';
+            this.startPosition[partName] = Object();
+            Object.keys(character[partName]).forEach((partType, i) => {
+                this.startPosition[partName][partType] = Object();
+                this.part[partName][partType] = createImage(character[partName][partType].src, null);
+                this.startPosition[partName][partType].left = this.part[partName][partType].style.left = `${character[partName][partType].position.x}px`;
+                this.startPosition[partName][partType].top = this.part[partName][partType].style.top = `${character[partName][partType].position.y}px`;
+                this.part[partName][partType].setAttribute('id', `anime-chan-${partName}`);
             });
         });
+        /**
+         * How much bigger or smaller has the image become on the page
+         * @type {Number}
+         */
         this.ratio = 1;
+    }
+
+    /**
+     * Sets active part of the main character on another part
+     * @function
+     * @memberof AnimeChan
+     * @param {!String} partName - Part of th character that you want to change
+     * @param {?String} partType - Name of the part type ('main' by default)
+     * @returns {boolean} - Success or failure
+     */
+    setActive = function (partName, partType) {
+        partType = partType || 'main';
+        try {
+            if (this.active[partName] == partType) return false;
+
+            this.active[partName] = partType;
+            (document.getElementById(containerId) || document.documentElement).replaceChild(
+                this.part[partName][partType],
+                document.getElementById(`anime-chan-${partName}`) || document.createElement('img')
+            );
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
     };
-};
+}
 
-const 
-    /**
-     * Path for all img sources
-     * @type {String}
-     */
-    path = './img/anime-chan/',
-    /**
-     * Anime character
-     * @type {AnimeChan}
-     */
-    animeChan = new AnimeChan({
-        sclera: {mainSrc : path + 'sclera.png', position : {x: 0, y:0}},
-        eyes: {mainSrc : path + 'eyes.png', position : {x: 595, y: 368}},
-        body: {mainSrc : path + 'body.png'},
-        mouth: {mainSrc : path + 'smile.png', position : {x: 653, y: 506}}
-    });
+/** Anime character @type {AnimeChan}*/
+globalThis.animeChan = new AnimeChan({
+    sclera: { main: { src: path + 'sclera.png', position: { x: 0, y: 0 } } },
+    eyes: { main: { src: path + 'eyes.png', position: { x: 595, y: 368 } } },
+    body: { main: { src: path + 'body.png', position: { x: 0, y: 0 } } },
+    mouth: {
+        main: { src: path + 'opened-mouth.png', position: { x: 695, y: 528 } },
+        //@ts-expect-error
+        smile: { src: path + 'smile.png', position: { x: 653, y: 506 } },
+        sad: { src: path + 'sad.png', position: { x: 698, y: 531 } },
+    },
+});
 
-['DOMContentLoaded', 'resize'].forEach(type => window.addEventListener(type, () => {
-    /**
-     * Container that will contain anime character
-     * @type {HTMLElement}
-     */
-    const container = document.getElementById('anime-chan-container') || document.documentElement,
-    containerParams = container.getBoundingClientRect(),
-    setSize = containerParams.width < containerParams.height? 'width' : 'height';
-    removeSize = setSize == 'height'? 'width' : 'height';
+['DOMContentLoaded', 'resize'].forEach((event) =>
+    window.addEventListener(event, () => {
+        /**
+         * Container that will contain anime character
+         * @type {HTMLElement}
+         */
+        const container = document.getElementById(containerId) || document.documentElement,
+            containerParams = container.getBoundingClientRect(),
+            minSize = containerParams.width < containerParams.height ? 'width' : 'height';
 
-    animeChan.ratio = animeChan.part.body.mainSrc[setSize] / containerParams[setSize];
-    Object.keys(animeChan.part).forEach(partName => {
-        Object.keys(animeChan.part[partName]).forEach(partImage => {
-            animeChan.part[partName][partImage].style[setSize] = 
-                `${animeChan.part[partName][partImage][setSize] / animeChan.ratio}px`;
-            animeChan.part[partName][partImage].style.removeProperty(removeSize);
-        });
-    });
-    ['top', 'left'].forEach(side => {
-        Object.keys(animeChan.part).forEach(partName => {
-            Object.keys(animeChan.part[partName]).forEach(partImage => {
-                animeChan.part[partName][partImage].style[side] = 
-                    `${parseFloat(animeChan.part[partName][partImage].style[side]) / animeChan.ratio}px`;
+        animeChan.ratio = animeChan.part.body.main[addPrefix('natural', minSize)] / containerParams[minSize];
+        ['width', 'height'].forEach((prop) => {
+            Object.keys(animeChan.part).forEach((partName) => {
+                Object.keys(animeChan.part[partName]).forEach((partImage) => {
+                    animeChan.part[partName][partImage].style[addPrefix('max', prop)] = (
+                        `${animeChan.part[partName][partImage][addPrefix('natural', prop)] / animeChan.ratio}px`
+                    );
+                });
             });
         });
-    });
 
-    Object.keys(animeChan.part).forEach(partName => {
-        if (animeChan.activeSrc[partName])
-            container.appendChild(animeChan.part[partName][animeChan.activeSrc[partName]]);
-    });
-}));
+        ['top', 'left'].forEach((side) => {
+            Object.keys(animeChan.part).forEach((partName) => {
+                Object.keys(animeChan.part[partName]).forEach((partImage) => {
+                    animeChan.part[partName][partImage].style[side] = `${parseFloat(animeChan.startPosition[partName][partImage][side]) / animeChan.ratio}px`;
+                });
+            });
+        });
+
+        Object.keys(animeChan.part).forEach((partName) => {
+            if (animeChan.active[partName]) container.appendChild(animeChan.part[partName][animeChan.active[partName]]);
+        });
+    })
+);
